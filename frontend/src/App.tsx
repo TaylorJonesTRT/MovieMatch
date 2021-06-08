8d2efeedfd314feca100141096567ee15a742fee
Taylor Jones <taylor@taylorwjones.com>
2021-06-07 21:16:14 -0500

    refactored fetchRandomMovie to only use movies that have a poster and that are also existing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect, useCallback, useReducer } from 'react';
-import './App.css';
+import './styles/App.css';
+import noPosterImg from './static/images/no_poster.png';
 
 // This will be used to force an update on the DOM/Components so that if a movie
 // is returned with a 404 error or is an Adult film the first useEffect below
@@ -16,8 +17,7 @@ function useForceUpdate() {
 function App() {
   const [latestMovieID, setLatestMovieID] = useState(0);
   const [randomMovieData, setRandomMovieData] = useState([]);
-
-  // const [ignored, forceUpdate] = useReducer((x) => x + 1, 0);
+  const [moviePoster, setMoviePoster] = useState('');
 
   const forceUpdate = useForceUpdate();
 
@@ -29,9 +29,7 @@ function App() {
       if (!movie.ok) {
         forceUpdate();
       }
-
       const data = await movie.json();
-
       if (data.adult) {
         forceUpdate();
       }
@@ -47,7 +45,22 @@ function App() {
         `https://api.themoviedb.org/3/movie/${randomMovieID}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
       );
 
+      // checking to make sure a movie was chosen and not an error, if error
+      // forcing update on app
+      if (!randomMovie.ok) {
+        forceUpdate();
+      }
+
       const movieData = await randomMovie.json();
+
+      // setting the poster image or forcing update if no poster
+      const imagePath = 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2';
+      if (movieData.poster_path === null) {
+        forceUpdate();
+      } else {
+        setMoviePoster(`${imagePath} + ${movieData.poster_path}`);
+      }
+
       setRandomMovieData(movieData);
     };
     fetchRandomMovie();
@@ -55,7 +68,16 @@ function App() {
 
   return (
     <div className="App">
-      <h1>hi</h1>
+      <div className="header">
+        <h1>MovieMatch</h1>
+        <div className="user-bar" />
+      </div>
+
+      <div className="content">
+        <div className="movie-card">movie name</div>
+        <div className="dislike-btn">D</div>
+        <div className="like-btn">L</div>
+      </div>
     </div>
   );
 }
