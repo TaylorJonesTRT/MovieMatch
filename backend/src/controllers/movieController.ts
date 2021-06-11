fd2ba9301ca450dc9dc343d51dc3b9acc75270fd
Taylor Jones <taylor@taylorwjones.com>
2021-06-10 21:24:44 -0500

    Conditionals for adult, no poster, and 404 created for movies to be skipped

--- backend/src/controllers/movieController.ts
+++ backend/src/controllers/movieController.ts
@@ -10,47 +10,33 @@ function waitFor(millSeconds: any): Promise<any> {
   });
 }
 
-// const fetchRandomMovie = async (url: any, options: any, n: number): Promise<any> => {
-//   try {
-//     const randomMovie = await fetch(url, options)
-//       .then((randomMovieData) => randomMovieData.json());
-//     if (randomMovie.adult) {
-//       console.log(randomMovie);
-//       console.log('Refetching as adult movie');
-//       await waitFor(1000 * n);
-//       return fetchRandomMovie(url, options, n - 1);
-//     }
-//     return randomMovie;
-//   } catch (err) {
-//     if (n === 1) {
-//       throw err;
-//     }
-
-//     await waitFor(1000 * n);
-//     return fetchRandomMovie(url, options, n - 1);
-//   }
-// };
-
 const fetchRandomMovie = async (options: any, n: number): Promise<any> => {
   try {
     const latestMovieID = await fetch(`https://api.themoviedb.org/3/movie/latest?api_key=${process.env.TMDB_API_KEY}`)
       .then((data) => data.json())
-      .then((movieData) => movieData.id)
+      .then((movieData) => movieData.id);
     const randomMovieID = Math.floor(Math.random() * latestMovieID);
     const randomMovieSelection = await fetch(`https://api.themoviedb.org/3/movie/${randomMovieID}?api_key=${process.env.TMDB_API_KEY}`)
       .then((data) => data.json());
 
-    if (!randomMovieSelection.ok) {
+    if (randomMovieSelection.status_code === 34) {
       console.log('404 error, refetching');
       await waitFor(100 * n);
       return fetchRandomMovie({}, n - 1);
     }
-
     if (randomMovieSelection.adult) {
       console.log('Refeteching due to adult movie');
       await waitFor(100 * n);
       return fetchRandomMovie({}, n - 1);
     }
+    // eslint-disable-next-line max-len
+    if (randomMovieSelection.poster_path === null || randomMovieSelection.poster_path === undefined) {
+      console.log('movie has no poster, skipping');
+      await waitFor(100 * n);
+      return fetchRandomMovie({}, n - 1);
+    }
+
+    // Successful movie grab
     return randomMovieSelection;
   } catch (err) {
     if (n === 1) {
