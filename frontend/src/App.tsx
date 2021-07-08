d64128d56fb02215228721eda804f044d3afadda
Taylor Jones <taylor@taylorwjones.com>
2021-07-07 20:42:58 -0500

    Progress on the UI, changing to utilizing both grid and flex fixed my issues

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart, faThumbsDown } from '@fortawesome/free-solid-svg-icons';
+import logo from './static/images/logo.png';
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -24,27 +25,36 @@ function App() {
   }, []);
 
   return (
-    <div className="App w-screen h-screen">
+    <div className="App w-screen h-screen bg-gradient-to-b from-gray-200 to-gray-50">
       {loading ? (
         <div> ...loading... </div>
       ) : (
         <>
-          <header className="w-full h-10 flex flex-row content-center p-1.5">
-            <h1 className="w-1/2 self-center">MovieMatch</h1>
+          <header className="w-full h-1/6 flex flex-row content-center p-2.5">
+            <div className="w-2/5 self-center">
+              <img src={logo} alt="MovieMatch" />
+            </div>
             <div className="user-bar w-1/2 self-center text-right">
               <h1>test user</h1>
             </div>
           </header>
 
-          <div className="content w-full">
-            <div className="movie-card h-5/6">
-              <img src={moviePoster} alt="movieName" />
+          <div className="content w-full h-5/6">
+            <div className="movie-card flex flex-col justify-center">
+              <img
+                src={moviePoster}
+                alt="movieName"
+                className="h-96 shadow-2xl"
+              />
+              <p className="poster-notice text-center text-xs text-gray-500">
+                Click the poster for details
+              </p>
             </div>
-            <div className="choices p-10 h-1/6 w-full grid grid-cols-2 grid-rows-1 grid-flow-row justify-items-center text-center place-content-center">
-              <div className="dislike-btn w-1/2 h-full text-red-400 text-5xl self-center">
+            <div className="choices w-full pt-10 text-5xl grid grid-cols-2 grid-rows-1 grid-flow-row justify-items-center text-center place-content-center">
+              <div className="dislike-btn text-red-400 self-center">
                 <FontAwesomeIcon icon={faThumbsDown} />
               </div>
-              <div className="like-btn h-full w-1/2 text-green-600 text-5xl">
+              <div className="like-btn text-green-600">
                 <FontAwesomeIcon icon={faHeart} />
               </div>
             </div>
