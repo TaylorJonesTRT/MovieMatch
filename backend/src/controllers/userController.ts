701b2eaf85864f43145b793215165e56df187a21
Taylor Jones <taylor@taylorwjones.com>
2021-07-10 21:54:02 -0500

    Starting on the userController

---
+++ backend/src/controllers/userController.ts
@@ -0,0 +1,10 @@
+import { body, validationRequest } from "express-validator";
+import Movie from '../models/movieModel';
+
+exports.showLikedMovies = function (req, res, next) {
+  // Not implemented yet
+};
+
+exports.saveMovie = [
+  // Not implemented yet
+]
