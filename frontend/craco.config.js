0b1f5cd4a02f3945f568d10f2874c80d486669b6
Taylor Jones <taylor@taylorwjones.com>
2021-07-28 14:24:05 -0500

    heroku setup

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,5 +1,13 @@
 // craco.config.js
+const path = require('path');
+
 module.exports = {
+  webpack: {
+    configure: (webpackConfig, { env, paths }) => {
+      paths.appBuild = webpackConfig.output.path = path.resolve('../public');
+      return webpackConfig;
+    },
+  },
   style: {
     postcss: {
       plugins: [require('tailwindcss'), require('autoprefixer')],
