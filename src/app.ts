f392baf5cf7ec189cbc71ca623fc967461189045
Taylor Jones <taylor@taylorwjones.com>
2021-07-28 16:27:52 -0500

    changes to app

--- src/app.ts
+++ src/app.ts
@@ -39,7 +39,10 @@ app.use('/api/', api);
 // app.get('/', (req: any, res: any) => {
 //   res.json({ message: 'Invalid Endpoint' });
 // });
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+app.get('/my-stuff', (req, res, next) => {
   res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
